51bfd473c114fabfb10025cb343c3a7ab519fb16
ar-siddiqui <abdulraheemsiddiqui@gmail.com>
2024-04-17 17:12:09 +0000

    Add logging level config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,17 +1,26 @@
 package config
 
+import (
+	"os"
+
+	log "github.com/sirupsen/logrus"
+)
+
 var GlobalConfig AppConfig
 
 type AppConfig struct {
-	CapitalizeName bool
-	Language       string
+	LogLevel log.Level
 }
 
 func LoadConfig() {
-	// Load your configurations into GlobalConfig
-	// GlobalConfig.CapitalizeName = strings.ToUpper(os.Getenv("GOCLIAPP_CAPITALIZE_NAME")) == "TRUE"
-	// GlobalConfig.Language = os.Getenv("GOCLIAPP_LANGUAGE")
-	// if GlobalConfig.Language == "" {
-	// 	GlobalConfig.Language = "English" // Default language
-	// }
+	// Load configurations into GlobalConfig
+
+	if os.Getenv("FLOWS2FIM_LOG_LEVEL") != "" {
+		lvl, err := log.ParseLevel(os.Getenv("FLOWS2FIM_LOG_LEVEL"))
+		if err != nil {
+			log.Warnf("Invalid LOG_LEVEL set: %s, defaulting to INFO", os.Getenv("FLOWS2FIM_LOG_LEVEL"))
+			lvl = log.InfoLevel
+		}
+		GlobalConfig.LogLevel = lvl
+	}
 }
